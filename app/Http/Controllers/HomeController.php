6edb9e16b91d480817eb0fed8091f4a9a16a2134
marisalerno <salerno.marisa@gmail.com>
2018-01-21 19:19:54 -0300

    Test with DatePicker

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,14 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+}
+
+class DateController extends Controller
+{
+    function showDate(Request $request)
+    {
+
+       dd($request->date);
     }
 }

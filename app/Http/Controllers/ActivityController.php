0911eef698eea62ee672ad73461b15bdf6fece1c
Marisa Salerno <salerno.marisa@gmail.com>
2018-03-03 19:54:58 -0300

    ABMActivity

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TeachingObject;
+use App\Activity;
+use Illuminate\Http\Request;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $activities = Activity::all();
+      return view('Activity.index',['activities' => $activities]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Activity.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $activity = Activity::create($request->all());
+      return redirect()->route('activity.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Activity $activity)
+    {
+        return view('Activity.show',['activity'=> $activity]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Activity $activity)
+    {
+        return view('Activity.update',['activity'=> $activity]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Activity $activity)
+    {
+      $activity->update($request->all());
+
+      return redirect()->route('activity.index');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Activity  $activity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Activity $activity)
+    {
+        $activity->delete();
+        return redirect()->route('activity.index');
+    }
+
+    private function getIds($objects)
+    {
+      $ids = [];
+      foreach ($objects as $object) {
+        $ids[] = $object->id;
+      }
+      return $ids;
+    }
+
+}

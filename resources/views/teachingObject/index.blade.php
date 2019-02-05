b5866d60fcfe3ab8f30065e2568affdd3f3116ef
marisalerno <salerno.marisa@gmail.com>
2019-02-04 21:27:27 -0300

    Show actions in TO according to the user

--- resources/views/teachingObject/index.blade.php
+++ resources/views/teachingObject/index.blade.php
@@ -17,18 +17,20 @@
                     <th>Acciones</th>
                   </tr>
                   @foreach ($teachingObjects as $teachingObject)
-                  <tr>
-                    <td><a href="{{route('teachingObject.show', $teachingObject)}}">{{$teachingObject->title}}</a></td>
-                    <td>{{$teachingObject->authorsNames()}}</td>
-                    <td>
-                      <a href="{{route('teachingObject.edit', $teachingObject)}}"><i class="fas fa-edit"></i></a>
-                      <form id="delete-teachingObject-{{$teachingObject->id}}" action="{{route('teachingObject.destroy', $teachingObject)}}" method="post">
-                        {{ csrf_field() }}
-                        <input name="_method" type="hidden" value="delete">
-                        <a href="#" onclick="document.getElementById('delete-teachingObject-{{$teachingObject->id}}').submit()"><i class="fas fa-trash"></i></a>
-                      </form>
-                    </td>
-                  </tr>
+                     <tr>
+                       <td><a href="{{route('teachingObject.show', $teachingObject)}}">{{$teachingObject->title}}</a></td>
+                       <td>{{$teachingObject->authorsNames()}}</td>
+                       <td>
+                         @if(in_array(Auth::user()->id,$teachingObject->authorsIds()))
+                           <a href="{{route('teachingObject.edit', $teachingObject)}}"><i class="fas fa-edit"></i></a>
+                           <form id="delete-teachingObject-{{$teachingObject->id}}" action="{{route('teachingObject.destroy', $teachingObject)}}" method="post">
+                             {{ csrf_field() }}
+                             <input name="_method" type="hidden" value="delete">
+                             <a href="#" onclick="document.getElementById('delete-teachingObject-{{$teachingObject->id}}').submit()"><i class="fas fa-trash"></i></a>
+                           </form>
+                         @endif
+                       </td>
+                     </tr>
                   @endforeach
                 </table>
             </div>
